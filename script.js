4600b2947e6b7f5058058d84c5ade592b055f545
IbrahimDarwesh <167519494+IbrahimDarwesh@users.noreply.github.com>
2024-06-18 15:59:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let day = prompt("Enter Name of the Day (Lower case)")
+if (day === "saturday" || day === "Sunday"){
+alert ("Weekenday");
+}
+else if (day == "monday" || day == "tudesday" || day == "wednesday" || day == "thursday"|| day == "friday" )
+ {
+    alert ("week day");
+}
+else {
+    alert("Invilde Entry");
+}
